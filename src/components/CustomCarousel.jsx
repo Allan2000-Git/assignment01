01be36028b8bcfaca6395b898a8f0792247d306f
Allan Joston Fernandes <allanfernandes459@gmail.com>
2023-11-25 12:51:43 +0530

    images

--- src/components/CustomCarousel.jsx
+++ src/components/CustomCarousel.jsx
@@ -20,28 +20,28 @@ const CustomCarousel = () => {
         afterChange: (current, next) => setActiveSlide2(current),
         responsive: [
             {
-            breakpoint: 968,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
+                breakpoint: 968,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
             },
             {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                initialSlide: 2
-            }
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
             },
             {
-            breakpoint: 375,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
+                breakpoint: 375,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
         ]
     };
