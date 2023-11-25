af1dd38ab7b2325009234f83f2cdd6eefbb0e9f6
Allan Joston Fernandes <allanfernandes459@gmail.com>
2023-11-25 12:18:56 +0530

    images folder

--- src/cards.js
+++ src/cards.js
@@ -1,37 +1,37 @@
 export const cards = [
     {
         "id":Date.now(),
-        "image":"../src/assets/img1.png",
+        "image":"../public/static/images/img1.png",
         "title":"Grilled  Tomatoes at Home",
         "description":"PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard..."
     },
     {
         "id":Date.now(),
-        "image":"../src/assets/img2.png",
+        "image":"../public/static/images/img2.png",
         "title":"Snacks for Travel",
         "description":"PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard..."
     },
     {
         "id":Date.now(),
-        "image":"../src/assets/img3.png",
+        "image":"../public/static/images/img3.png",
         "title":"Post-workout Recipes",
         "description":"PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard..."
     },
     {
         "id":Date.now(),
-        "image":"../src/assets/img4.png",
+        "image":"../public/static/images/img4.png",
         "title":"How To Grill Corn",
         "description":"PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard..."
     },
     {
         "id":Date.now(),
-        "image":"../src/assets/img5.png",
+        "image":"../public/static/images/img5.png",
         "title":"Crunchwrap Supreme",
         "description":"PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard..."
     },
     {
         "id":Date.now(),
-        "image":"../src/assets/img6.png",
+        "image":"../public/static/images/img6.png",
         "title":"Broccoli Cheese Soup",
         "description":"PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard..."
     }
